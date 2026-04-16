24038c26046afb75f27112a05f853fd21119b57e
Najmun Nahar Anjum <najmunnanjum121@gmail.com>
2026-04-17 02:44:01 +0600

    stats page done

--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -1,9 +1,107 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useFriends } from '../context/FriendContex';
+import { PieChart, Pie, Cell, Sector, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const Stats = () => {
+    const { timeline = [] } = useFriends();
+
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    const callCount = timeline.filter(t => t.type === "Call").length;
+    const textCount = timeline.filter(t => t.type === "Text").length;
+    const videoCount = timeline.filter(t => t.type === "Video").length;
+
+    const data = [
+        { name: "Text", value: textCount, fill: "#7F37F5" },
+        { name: "Call", value: callCount, fill: "#244d3f" },
+        { name: "Video", value: videoCount, fill: "#37A163" },
+    ];
+
+
+    const renderActiveShape = (props) => {
+        const RADIAN = Math.PI / 180;
+        const {
+            cx, cy, midAngle, innerRadius, outerRadius,
+            startAngle, endAngle, fill, payload, percent, value
+        } = props;
+
+        const sin = Math.sin(-RADIAN * midAngle);
+        const cos = Math.cos(-RADIAN * midAngle);
+
+        const sx = cx + (outerRadius + 10) * cos;
+        const sy = cy + (outerRadius + 10) * sin;
+        const mx = cx + (outerRadius + 30) * cos;
+        const my = cy + (outerRadius + 30) * sin;
+
+        return (
+            <g>
+                <text x={cx} y={cy} dy={8} textAnchor="middle" fill="#333">
+                    {payload.name}
+                </text>
+
+                <Sector
+                    cx={cx}
+                    cy={cy}
+                    innerRadius={innerRadius}
+                    outerRadius={outerRadius + 10}
+                    startAngle={startAngle}
+                    endAngle={endAngle}
+                    fill={fill}
+                />
+            </g>
+        );
+    };
+
     return (
-        <div>Stats</div>
+        <div className='bg-[#f8fafc] w-full  flex flex-col'>
+
+
+            <h1 className='text-2xl w-[95%] sm:w-[85%] lg:w-[70%]  mx-auto sm:text-4xl font-bold my-5 text-[#1F2937]'>
+                Friendship Analytics
+            </h1>
+
+            <div
+                className="
+        bg-white 
+        w-[95%] sm:w-[85%] lg:w-[70%] 
+        mx-auto mt-5 mb-10
+        shadow-md rounded-xl 
+        p-4 sm:p-6
+    "
+            >
+                <h2 className="text-xl font-semibold text-[#1F2937] mb-4">
+                    By Interaction Type
+                </h2>
+
+                <div className="w-full h-[350px] flex justify-center">
+                    <ResponsiveContainer width="100%" height="100%">
+                        <PieChart>
+                            <Pie
+                                activeIndex={activeIndex}
+                                activeShape={renderActiveShape}
+                                data={data}
+                                cx="50%"
+                                cy="50%"
+                                innerRadius="45%"
+                                outerRadius="70%"
+                                dataKey="value"
+                                onMouseEnter={(_, index) => setActiveIndex(index)}
+                            >
+                                {data.map((entry, index) => (
+                                    <Cell key={index} fill={entry.fill} />
+                                ))}
+                            </Pie>
+
+                            <Tooltip />
+                            <Legend />
+                        </PieChart>
+                    </ResponsiveContainer>
+                </div>
+            </div>
+
+
+        </div >
     )
 }
 
-export default Stats
+export default Stats; 
--- src/pages/Timeline.jsx
+++ src/pages/Timeline.jsx
@@ -30,17 +30,17 @@ const Timeline = () => {
     };
 
     return (
-        <div className='bg-[#f8fafc] flex flex-col min-h-screen '>
+        <div className='bg-[#f8fafc] flex flex-col min-h-screen'>
 
             <div className="w-[70%] mx-auto  my-6 p-4 space-y-6">
 
-                <h1 className='font-bold text-[48px]'>Timeline</h1>
+                <h1 className='font-bold text-[#1F2937] text-[48px]'>Timeline</h1>
 
                 <div className="flex  mb-6">
                     <select
                         value={filter}
                         onChange={(e) => setFilter(e.target.value)}
-                        className="border border-gray-300 rounded px-3 py-2 bg-white text-[#64748b] focus:outline-none"
+                        className="border border-gray-300  w-[300px] rounded px-3 py-2 bg-white text-[#64748b] focus:outline-none"
                     >
                         <option value="All">Filter timeline</option>
                         <option value="Call">Call</option>
@@ -54,8 +54,8 @@ const Timeline = () => {
 
                 {filteredTimeline.length === 0 ? (
                     <div className="flex justify-center">
-                        <div className="bg-white shadow-sm rounded-xl p-10 text-center w-full mt-10 md:w-[60%] lg:w-[60%]">
-                            <p className="text-gray-500 text-[48px] font-medium">
+                        <div className="bg-white shadow-sm rounded-xl p-10 text-center w-full my-30 md:w-[60%] lg:w-[60%]">
+                            <p className="text-gray-500 md:text-[48px] text-[24px]  font-medium">
                                 No activity yet
                             </p>
                         </div>
